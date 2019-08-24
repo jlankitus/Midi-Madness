af337f4153bd693dbec884eb70ff9053522f8fc2
Jed Lankitus <jlankitus@gmail.com>
2019-08-24 11:59:52 -0700

    First Commit!

---
+++ Midi_Madness/Assets/floatCatcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatCatcher : MonoBehaviour
+{
+
+    public float value;
+
+    public void setValue(float val)
+    {
+        value = val;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
